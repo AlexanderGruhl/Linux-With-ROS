d6f0433449f8c8813bd071675e1f58f2cc8186e5
AlexanderGruhl <71097623+AlexanderGruhl@users.noreply.github.com>
2020-09-14 02:34:34 -0400

    Added Linux Course Files

---
+++ linux_course_files/move_bb8_pkg/my_scripts/ex_3.3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo Going to my_scripts folder
+cd ~/catkin_ws/src/linux_course_files/move_bb8_pkg/my_scripts
+echo Checking permissions
+ls -la
+chmod 777 move_bb8_square.py
+echo Rechecking permissions
+ls -la
+
